3ce37d14db431d8649a2d3eda2a39e566ab93733
Rodrigo <rodrigo@Rodrigos-MacBook-Pro.local>
2021-07-27 00:33:17 -0300

    Update on JaxrsConfig.java

--- src/main/java/org/openshift/JaxrsConfig.java
+++ src/main/java/org/openshift/JaxrsConfig.java
@@ -1,7 +1,7 @@
 package org.openshift;
 
 import javax.ws.rs.ApplicationPath;
-import javax.ws.rd.core.Application;
+import javax.ws.rs.core.Application;
 
 @ApplicationPath("/api")
 public class JaxrsConfig extends Application {
